e7247a55b96846b87fb156cd4025ed048118e269
Prabakaran <prabakaranpkv@gmail.com>
2021-10-15 11:18:14 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Login from "./components/Login";
+import Register from "./components/register/Register";
+import ResetPassword from "./components/ResetPassword";
+import ForgotPassWord from "./components/ForgotPassword";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/signup">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/forgot-password">
+          <ForgotPassWord />
+        </Route>
+        <Route exact path="/reset-password/:resetToken">
+          <ResetPassword />
+        </Route>
+      </Switch>
     </div>
   );
 }
